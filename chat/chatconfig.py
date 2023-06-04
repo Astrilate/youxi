920e908414f1d1e53dbb3eda864a86198f77cac1
ThreeBodies928 <121529104+ThreeBodies928@users.noreply.github.com>
2023-06-04 17:09:22 +0800

    Add files via upload

---
+++ chat/chatconfig.py
@@ -0,0 +1,18 @@
+from app.chat.config import HOST
+from app.config import PORT
+
+private_chats = {}  # 私聊对话池
+
+chat_history = []  # 存储聊天记录的列表
+
+error_history = []  # 错误日志
+
+connections = {}  # 全局链接字典
+
+clients = {}  # 存储连接的客户端信息的字典
+
+connected_users = {}  # 存储已连接的用户信息
+
+SERVER_PORT = PORT
+
+SERVER_ADDRESS = HOST
