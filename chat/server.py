febaa7b17cd02dcf272e90f79ff437cecc85a933
ThreeBodies928 <121529104+ThreeBodies928@users.noreply.github.com>
2023-06-04 17:34:26 +0800

    Update server.py

--- chat/server.py
+++ chat/server.py
@@ -1,10 +1,11 @@
 import asyncio
+import json
 import urllib
 from urllib.parse import urlparse
 
 import websockets.exceptions
 
-from app.chat.chat import send_private_message, create_private_message, my_send, handle_message
+from app.chat.chat import send_private_message, create_private_message, my_send, handle_message, make_message
 from app.chat.chatconfig import connections, SERVER_PORT
 from app.chat.connection import handle_new_connection, handle_connection_closed, handle_connection
 
@@ -43,7 +44,11 @@ async def start_server():
                 query_params = urllib.parse.parse_qs(request.path)
                 chat_connection_id = query_params.get('chat_connection_id', [''])[0]
                 send_id = query_params.get('send_id', [''])[0]
-                message = query_params.get('message', [''])[0]
+                # message = query_params.get('message', [''])[0]
+                # 解析客户端的请求参数（JSON 传参）
+                query_string = request.query_string.decode('utf-8')
+                query_message = json.loads(query_string)
+                message = query_message.get('content', '')
                 await send_private_message(chat_connection_id, send_id, message)
             elif path == "/chat/create":
                 # 获取 WebSocket 连接的请求对象
@@ -58,7 +63,11 @@ async def start_server():
                 request = client_websocket.request
                 # 解析客户端的请求参数（地址传参
                 query_params = urllib.parse.parse_qs(request.path)
-                message = query_params.get('message', [''])[0]
+                # message = query_params.get('message', [''])[0]
+                # 解析客户端的请求参数（JSON 传参）
+                query_string = request.query_string.decode('utf-8')
+                query_message = json.loads(query_string)
+                message = query_message.get('content', '')
                 chat_id = query_params.get('chat_id', [''])[0]
                 await my_send(chat_id, message)
             # await keep_handle_message(client_websocket)
@@ -66,5 +75,14 @@ async def start_server():
             # await handle_delete_message(client_websocket, client_websocket.path)
             # elif path == "/notification":
             # await show_notification()
-        except:
-            print("Invalid path")
+        except KeyError as e:
+            error = f"Error: {str(e)}"
+            print(error)
+            # error_message = make_message(send_id, 1, 2, error, datetime.datetime.now().timestamp())
+            # await my_send(send_id, error_message)
+
+        except Exception as e:
+            error = f"Path error: {str(e)}"
+            print(error)
+            # error_message = make_message(send_id, 1, 2, error, datetime.datetime.now().timestamp())
+            # await my_send(send_id, error_message)
